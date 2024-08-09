c1128754330e7ca2ccf67259081ddbb4c3cc8716
Brianmaina909 <152210115+Brianmaina909@users.noreply.github.com>
2024-08-09 17:18:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 import numpy as np
 
 # Display header images
-st.image(['Insurers.png', 'background image 2.jpeg'], width=600, use_column_width='auto')
+st.image(['Insurers.png', 'background image.jpeg'], width=600, use_column_width='auto')
 
 st.markdown("""
     <style>
