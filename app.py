5a671dd39ab330dda7dd26404e4522ee14485404
Brianmaina909 <152210115+Brianmaina909@users.noreply.github.com>
2024-08-09 17:14:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ st.markdown("""
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-position: center;
-        background-color: lightblue;
         color: black;
     }
     h1, h2, h3 {
